cddefe02e9e06f3ae3133bd34faad4a03becdc6a
shreegowri <gaganandh@gmail.com>
2022-06-20 13:23:41 +0530

    [shreegowri]find maximum of the integer array

--- Day13_FindMax/Day13_FindMax/Program.cs
+++ Day13_FindMax/Day13_FindMax/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("welcome to fine max problem");
+﻿namespace Day13_FindMax
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum value finder");
+            // MaxNumberCheck maxNumberCheck = new MaxNumberCheck();
+            int result = MaxNumberCheck.MaxIntegerNumber(4, 6, 2);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
